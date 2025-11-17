0de13f3606d0cf2709b6ea7d831647e99ad787d0
WiizMark <1DAM@A11-PC09>
2025-11-17 12:02:20 +0100

    Hecho Funciones2

--- Reto1/src/reto/Funciones2.java
+++ Reto1/src/reto/Funciones2.java
@@ -1,5 +1,30 @@
 package reto;
 
 public class Funciones2 {
-
+	
+	/*
+	 * El otro alumno se encargará de las operaciones (multiplicar, dividir) y de
+	 * los métodos (esPositivo y esNegativo).
+	 */
+	public static int multiplicar(int a, int b) {
+		return a * b;	
+	}
+	public static int dividir(int c, int d) {
+		return c / d;
+	}
+	
+	public static boolean esPositivo (int numero) {
+		if (numero >= 0) {
+			return true;
+		} else {
+			return false;
+	}
+}
+	public static boolean esNegativo (int num) {
+		if (num <= 0) {
+			return true;
+		} else {
+			return false;
+		}	
+	}
 }
