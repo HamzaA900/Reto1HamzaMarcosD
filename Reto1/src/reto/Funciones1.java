83563ebf4f6361b5d20d37ed695f7b4437b00a03
HamzaA900 <1DAM@A11-PC10>
2025-11-17 12:55:22 +0100

    Merge branch 'main' of https://github.com/HamzaA900/Reto1HamzaMarcosD.git into main

--- Reto1/src/reto/Funciones1.java
+++ Reto1/src/reto/Funciones1.java
@@ -1,5 +1,31 @@
 package reto;
 
 public class Funciones1 {
+	/*Un alumno se encargará de las operaciones básicas (sumar, restar) y de los métodos de
+	verificación de propiedades de números (esPar y esImpar).*/
+	public static int sumar(int a, int b) {
+		return a + b;
+		
+	}
+	public static int restar(int a, int b) {
+		return a - b;
+		
+	}
+	public static boolean esPar(int n) {
+		if (n%2==0) {
+			return true;
+		}else {
+			return false;
+		}
+		
+	}
+	public static boolean esImpar(int n) {
+		if (n%2!=0) {
+			return true;
+		}else {
+			return false;
+		}
+		
+	}
 
 }
